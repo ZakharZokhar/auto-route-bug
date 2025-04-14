0fecdf68e9edf9dd22eae701ccdd265149ddd58f
Zakhar Noviokv <zakharzokhar@gmail.com>
2025-04-14 16:39:09 +0700

    removed odd routes

--- lib/router.dart
+++ lib/router.dart
@@ -1,6 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:auto_route_test/main.dart';
-import 'package:flutter/material.dart';
 
 part 'router.gr.dart';
 
@@ -8,20 +7,7 @@ part 'router.gr.dart';
 class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => [
-    // AutoRoute(
-    //   path: '/',
-    //   page: AppWrapperRoute.page,
-    //   initial: true,
-    //   children: [
     AutoRoute(path: '/first', page: FirstRoute.page, initial: true),
     AutoRoute(path: '/second', page: SecondRoute.page),
-    // AutoRoute(path: 'register', page: RegisterRoute.page),
-    // AutoRoute(path: 'google-auth', page: GoogleAuthRoute.page),
-    // // AutoRoute(path: 'videos', page: VideoListRoute.page),
-    // AutoRoute(path: 'video', page: VideoRoute.page),
-    // AutoRoute(path: 'record', page: AudioRecorderRoute.page),
-    //   ],
-    // ),
-    // AutoRoute(path: '/videos', page: VideoListRoute.page),
   ];
 }
